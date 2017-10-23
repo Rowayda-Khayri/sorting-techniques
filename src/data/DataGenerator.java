7cf54dd10a2d92b7f8b72d6270b2bb7887039c42
Rowayda Khayri <rowayda.khayri@gmail.com>
2017-10-23 13:15:02 +0200

    implement generateRandomData method

--- src/data/DataGenerator.java
+++ src/data/DataGenerator.java
@@ -5,10 +5,42 @@
  */
 package data;
 
+import java.util.*;
+
 /**
  *
  * @author row
  */
 public class DataGenerator {
+    
+    
+    public String[] generateRandomData(){
+        
+        int counter ;
+        Random randomnum = new Random();
+        
+        //declare string obj to accept any data type
+        
+        String[] generatedData = new String[1500];
+        
+        //generate 1500 random numbers between 0 and 9999
+        
+        for (counter = 0; counter < 1500; counter++) {
+            
+            int rand  = randomnum.nextInt(9999);
+        
+            //convert int to string to be stored in string array 
+            String convertedInt = new String();
+            convertedInt = Integer.toString(rand);
+            generatedData[counter]  = convertedInt;
+
+        }
+//        System.out.println(generatedData.length); //1500
+
+       return generatedData; 
+
+    } 
+    
+    
     
 }
